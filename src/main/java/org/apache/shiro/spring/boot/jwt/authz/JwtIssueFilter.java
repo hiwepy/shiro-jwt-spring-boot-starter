15d137351ade0cb8cd39d1f15471ab5d6c17573e
vindell <hnxyhcwdl1003@163.com>
2018-08-19 22:09:36 +0800

    修改

--- src/main/java/org/apache/shiro/spring/boot/jwt/authz/JwtIssueFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/jwt/authz/JwtIssueFilter.java
@@ -1,5 +1,10 @@
 package org.apache.shiro.spring.boot.jwt.authz;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
@@ -10,6 +15,9 @@ import org.apache.shiro.spring.boot.jwt.token.JwtFactory;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.AccessControlFilter;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
+
 /**
  * Jwt 签发 (issue)过滤器 
  * @author 		： <a href="https://github.com/vindell">vindell</a>
@@ -17,6 +25,8 @@ import org.apache.shiro.web.filter.AccessControlFilter;
 public final class JwtIssueFilter extends AccessControlFilter {
 
 	private JwtFactory jwtFactory;
+	private String signingKey;
+	private String issuer;
 	
 	@Override
 	public boolean onPreHandle(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
@@ -24,11 +34,19 @@ public final class JwtIssueFilter extends AccessControlFilter {
 		Subject subject = SecurityUtils.getSubject();
 		if (subject == null || !subject.isAuthenticated()) {
 			
-			jwtFactory.issueJwt(signingKey, id, subject, issuer, period, roles, permissions, algorithm);
+			// 签发一个Json Web Token
+	        // 令牌ID=uuid，用户=clientKey，签发者=clientKey
+	        // token有效期=1分钟，用户角色=null,用户权限=create,read,update,delete
+	        String jwt = jwtFactory.issueJwt(signingKey, UUID.randomUUID().toString(), "", 
+	                                    "token-server", 60000l, null, "create,read,update,delete");
+	        
+	        Map<String,Object> data = Maps.newHashMap();
+	        data.put("status", HttpStatus.SC_OK);
+	        data.put("jwt", jwt);
 			
 			if (WebUtils.isAjaxRequest(request)) {
 				// 响应成功状态信息
-		        WebUtils.writeJSONString(response, HttpStatus.SC_SESSION_TIMEOUT, "Session Timeout.");
+		        WebUtils.writeJSONString(response, data);
 				return false;
 			}
 		}
--- src/main/java/org/apache/shiro/spring/boot/jwt/token/JwtFactory.java
+++ src/main/java/org/apache/shiro/spring/boot/jwt/token/JwtFactory.java
@@ -2,6 +2,22 @@ package org.apache.shiro.spring.boot.jwt.token;
 
 public interface JwtFactory {
 
-    public abstract String issueJwt(String signingKey,String id, String subject, String issuer, Long period, String roles, String permissions,
-    		String algorithm) throws Exception;
+	/**
+	 * 
+	 * 生成jwt签名
+	 * @author 		： <a href="https://github.com/vindell">vindell</a>
+	 * @param signingKey
+	 * @param id
+	 * @param subject
+	 * @param issuer
+	 * @param period
+	 * @param roles
+	 * @param permissions
+	 * @param algorithm
+	 * @return
+	 * @throws Exception
+	 */
+	public abstract String issueJwt(String signingKey, String id, String subject, String issuer, Long period,
+			String roles, String permissions, String algorithm) throws Exception;
+	
 }
